a91a055188b9546a32732bfad7b2cb1e457ffa1b
Archit Sachdeva <archit.sachdeva007@gmail.com>
2024-12-09 02:32:44 +0000

    fixed an error by removing unecessary file (i hope).

--- example.py
+++
@@ -1,9 +0,0 @@
-@app.route('/admin/manage', methods=['GET'])
-@login_required
-def admin_manage():
-    if not current_user.is_admin and current_user.email != os.environ.get('SUPER_ADMIN_EMAIL'):
-        flash('Access denied')
-        return redirect(url_for('home'))
-    users = User.query.all()
-    return render_template('admin_manage.html', users=users)
-
